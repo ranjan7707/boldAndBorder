5c0395cef79650ed9621fc4dbc65596b6370f55b
ranjan7707 <ranjan7707@gmail.com>
2023-04-24 00:07:27 +0530

    Bold

---
+++ dom.js
@@ -0,0 +1,6 @@
+//var headerTitle=document.getElementById('header-title');
+var header = document.getElementById('main-header');
+header.style.borderBottom= 'solid 3px #000';
+
+var titles=document.querySelectorAll('.title');
+titles[0].style.color='green'
