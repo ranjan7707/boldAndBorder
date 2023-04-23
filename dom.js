44ea24826d11b5079f5ec3978ed49a96d9b7f390
ranjan7707 <ranjan7707@gmail.com>
2023-04-24 00:42:02 +0530

    addNewItems

--- dom.js
+++ dom.js
@@ -1,15 +1,3 @@
-var items = document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor = 'green';
-//items[2].textContent = "Changed Name";
-//items[2].style.fontWeight = 'bold';
-
-for(var i = 0; i < items.length; i++){
-    items[i].style.fontWeight = 'bold';
-}
-
-var item = document.querySelector('.list-group-item');
-//item.style.color = 'green';
-
-for(var i = 0; i < items.length; i++){
-    items[i].style.color = 'yellow';
-}
+var NewItem= document.querySelector('.list-group-item:nth-child(5)');
+NewItem.style.color='blue';
+NewItem.style.backgroundColor= 'red';
